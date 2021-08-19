c8fcca8b8c6e0612a3d14e59e9f6abeb3937074a
Blankeos <carloantonioct@gmail.com>
2021-08-19 20:01:46 +0800

    Redoing everything in multiprocessing instead of threading

--- PokeMMO AutoBattler/main.py
+++ PokeMMO AutoBattler/main.py
@@ -1,49 +1,68 @@
 import time
-import threading
+import multiprocessing
 from pynput import keyboard as kb
 
-# Threads
-import automation
 
-# States
-programIsRunning = True
-botIsRunning = True
-
-
-def on_press(key):
-    global programIsRunning
-    global botIsRunning
-
-    if key == kb.Key.esc:
-        print("Program is terminating...")
-        programIsRunning = False
-        return False
-    elif key == kb.KeyCode.from_char("s"):
-        botIsRunning = True
-    elif key == kb.KeyCode.from_char("p"):
-        botIsRunning = False
+def do_something():
+    print("Sleeping 1 second...")
+    time.sleep(1)
+    print("Done Sleeping...")
 
 
-def the_thread():
-    global programIsRunning
-    global botIsRunning
-    while programIsRunning:
-        while not botIsRunning and programIsRunning:
-            time.sleep(0)
-        if not programIsRunning:
-            break
-        automation.run()
-        print(botIsRunning)
+if __name__ == '__main__':
+
+    start = time.perf_counter()
+
+    p1 = multiprocessing.Process(target=do_something)
+    p2 = multiprocessing.Process(target=do_something)
+
+    p1.start()
+    p2.start()
+
+    finish = time.perf_counter()
+
+    print(f'Finished in {round(finish-start, 2)} second(s)')
+
+# # Threads
+# import automation
+
+# # States
+# programIsRunning = True
+# botIsRunning = True
 
 
-# Main Automation Thread
-my_thread = threading.Thread(target=the_thread)
-my_thread.start()
+# def on_press(key):
+#     global programIsRunning
+#     global botIsRunning
 
-# Input Thread (Collects Keyboard Events)
-input_thread = kb.Listener(on_press=on_press)
-input_thread.start()
+#     if key == kb.Key.esc:
+#         print("Program is terminating...")
+#         programIsRunning = False
+#         return False
+#     elif key == kb.KeyCode.from_char("s"):
+#         botIsRunning = True
+#     elif key == kb.KeyCode.from_char("p"):
+#         botIsRunning = False
 
-# Join the threads to make them block
-my_thread.join()
-input_thread.join()
+
+# def the_thread():
+#     global programIsRunning
+#     global botIsRunning
+#     while programIsRunning:
+#         while not botIsRunning and programIsRunning:
+#             time.sleep(0)
+#         if not programIsRunning:
+#             break
+#         automation.run()
+
+# # Main Automation Thread
+# my_thread = threading.Thread(target=the_thread)
+# my_thread.start()
+
+# # Input Thread (Collects Keyboard Events)
+# input_thread = kb.Listener(on_press=on_press)
+# input_thread.start()
+
+# # Join the threads to make them block
+# my_thread.join()
+# input_thread.join()
